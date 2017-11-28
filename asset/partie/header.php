cd28155eb96694eadb80367efb5b89e15afc3684
matthieu <matthieu.tabarin@gmail.com>
2017-11-28 08:39:06 +0100

    Bootstrap + refonte css

--- asset/partie/header.php
+++ asset/partie/header.php
@@ -1,18 +1,18 @@
 <title><?= $title; ?></title>
 	<meta name="description" content="<?= $description; ?>"/>
 	<meta name="viewport" content="initial-scale=1"/>
-<link rel="stylesheet" href="<?= RACINE ?>asset/css/knacss.css">
+<link rel="stylesheet" href="<?= RACINE ?>asset/css/style.css">
 </head>
 <body>
 <header>
-			<div id="menu-hamburger" class="flex-container open">
+			<!-- <div id="menu-hamburger" class="flex-container open">
 				<p><i class="fa fa-bars fa-3x" aria-hidden="true"></i></p>
-			</div>
-			<nav class="flex-container-v menu">
-				<i class="fa fa-times fa-3x close" aria-hidden="true"></i>
-				<a href="<?= RACINE ?>">Accueil</a>
-				<a href="<?= RACINE ?>article.php">Portfolio</a>
-				<a href="#" class="contact btn-contact">Contact</a>
-				<a href="<?= RACINE ?>mentions_legales.php">Mentions légales</a>
+			</div> -->
+			<nav class="d-flex flex-row justify-content-center py-2">
+				<a href="<?= RACINE ?>" class="p-2">Accueil</a>
+				<a href="<?= RACINE ?>article.php" class="p-2">Portfolio</a>
+				<a href="#" class="p-2">Mon parcours</a>
+				<a href="#" class="p-2">Contact</a>
+				<a href="<?= RACINE ?>mentions_legales.php" class="p-2">Mentions légales</a>
 			</nav>
 		</header>
--- index.php
+++ index.php
@@ -1,33 +1,64 @@
 <?php
 include 'asset/partie/header_meta.php';
-$title = "Krav Webdesigner: Designer Web Freelance";
+$title = "Designer Web Freelance";
 $description = "Web design, Paris et Île de France, refonte de site, création de logo ou de charte graphique et référencements.";
 include 'asset/partie/header.php'; ?>
 
-<section class="flex-container-v main-content">
-	<div class="flex-item-center txtcenter slogan w50">
-		<img src="asset/img/logo-krav.svg" class="logo" alt="logo" width="40%">
-		<h1 class="graphiste">Graphiste  Webdesigner </h1>
-		<p></p>
-		<div class="grid competence">
-			<div>
-				<img src="asset/img/web-developpement.png" alt="design">
-				<h2>Charte graphique</h2>
+<section class="bg-light py-5">
+	<article class="container">
+		<div class="d-flex flex-column" style="height: 670px;">
+			<div class="text-center my-auto">
+				<img src="asset/img/logo-krav.png" class="img-fluid w-50" alt="logo graphiste web">
+				<h1>UX/UI design</h1>
+				<a href="#" class="btn btn-outline-primary font-weight-bold mt-3">Voir mes créations</a>
+			</div>
+		</div>
+	<article>
+</section>
+<section class="bg-dark">
+	<article class="container">
+		<div class="row text-center text-light py-4">
+			<div class="col-12 col-md-4">
+				<img src="asset/img/graphic-design.png" alt="design" class="text-white">
+				<h2 class="h3">Ergonomie</h2>
 				<p>Définissons ensemble une identité visuelle qui vous ressemble.</p>
 			</div>
-			<div>
-				<img src="asset/img/graphisme.png" alt="sketching">
-				<h2>Flyers & Carte de visite</h2>
+			<div class="col-12 col-md-4">
+				<img src="asset/img/vector.png" alt="sketching">
+				<h2 class="h3">Précision</h2>
 				<p>Mettez en avant vos évenements ou votre entreprise.</p>
 			</div>
-			<div>
-				<img src="asset/img/palette.png" alt="palette">
-				<h2>Web & Impression</h2>
+			<div class="col-12 col-md-4">
+				<img src="asset/img/rocket.png" alt="palette">
+				<h2 class="h3">Optimisation</h2>
 				<p>Nous travaillons aussi bien en ligne qu'en impression.</p>
 			</div>
 		</div>
-		<a href="article.php" class="btn work">Voir mon travail</a>
-		<a href="#" class="btn work btn-contact">Contactez moi</a>
+	</article>
+</section>
+<section class="bg-light">
+	<article class="container">
+		<div class="d-flex flex-row justify-content-center navbar py-2">
+			<a href="#" class="p-2">Graphisme</a>
+			<a href="#" class="p-2">Web</a>
+		</div>
+		<div class="row py-5">
+			<div class="col-12">
+				<h1 class="text-center">Insert Galerie</h1>
+			</div>
+		</div>
+	</article>
+</section>
+<section class="container-fluid">
+	<div class="row">
+	<article class="col-12 col-md-7 p-5">
+		<h2>Qui suis-je ?</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore consequuntur modi expedita ut ratione mollitia minus, perferendis beatae ipsa tenetur! Est mollitia quibusdam autem sit magnam, nesciunt nulla, nostrum magni molestias iure explicabo optio suscipit et, veritatis quo ipsum temporibus?</p>
+		<a href="#" class="btn btn-outline-primary">En savoir plus</a>
+	</article>
+	<article class="col-12 col-md bg-primary">
+
+	</article>
 	</div>
 </section>
 
