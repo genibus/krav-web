8b05fdbb9180cacdd8bc8b13563fed3bbe2af0c1
matthieu <matthieu.tabarin@gmail.com>
2018-01-03 22:53:06 +0100

    wip

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ include 'asset/partie/header.php';
 			<div class="col-12 col-md-10">
 			<a href="view.php?slug=<?= $article['work_url']?>" class="text-light text-decoration-n">
 					<div class="card mb-4">
-						<div class="card-body bg-img" style="background: linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)), url('asset/img/upload/<?= $article['nom_image'] ?>'); background-size: cover;">
+						<div class="card-body bg-img" style="background: linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)), url('asset/img/upload/<?= $article['nom_image'] ?>'); background-size: cover; background-position: center;">
 							<div class="row align-items-center" style="height:270px;">
 								<div class="col">
 									<h2 class="text-center text-uppercase font-weight-bold"><?=  $article['work_name'];?></h2>
